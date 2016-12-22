393274d8a9a49af48ba3624faea6e34d9e09dbf0
jasontr <jasonacg@gmail.com>
2016-12-22 09:17:14 +0900

    add plot code

--- plot/test.m
+++ plot/test.m
@@ -1,9 +1,50 @@
 function a = test()
+addpath(pwd);
+rpath = 'C:\Users\jason_000\Documents\coding\Codes_of_master_thesis\plot\plot_data\';
+cd plot_data
+for OS = 10:10:30
+    cd (num2str(OS));
+    ha = tight_subplot(1,10,[.0 .0],[.1 .01],[.04 .04]);
+    for beta = 0:9
+        cd(num2str(beta))
+        path = pwd;
+        subpath = fullfile( path, '*.mat' );
+        mat = dir (subpath);
+        pdata = struct();
+        for tensorsize = 100:50:200
+            tensorsetting = [num2str(tensorsize), '_10_', num2str(OS)];
+            [L, newL] = create_beta_name(tensorsetting, beta);
+            pdata = readdata(L, newL, pdata);
+        end
+        axes(ha(beta+1)); 
+        
+        str = ['\beta = ', num2str(beta*0.2)];
+        dim = [.2 .5 .3 .3];
+        %annotation('textbox',dim,'String',str,'FitBoxToText','on');
+        %text(ha(beta+1),.5,.9,str);
+        name = {'100';'150';'200'};
+        y = [pdata.test_error]; 
+        bar(y)
+        ylim([0 1])
+        cd ..;
+    end
+    legend1 = legend(ha(10),'L','newL');
+    set(legend1,'FontSize',14);
+    set(ha(1:10),'XTickLabel',name); 
+    set(ha(2:10),'YTickLabel','');
+    cd ..;
+    pause;
+end
+cd ..;
+a = 0;
+
+
+end
+
+function plot_f()
 
 ha = tight_subplot(1,6,[.0 .0],[.1 .01],[.04 .04]);
-path = 'C:\Users\jason_000\Documents\coding\Codes_of_master_thesis\Tensor_completion\data_beta_v2\100_5_10\';
-load((filemat), 'problem_description');
-for ii = 1:5; 
+for ii = 1:10; 
     axes(ha(ii)); 
     name = {'m1';'m2'};
     y = [19,24;12,14]; 
@@ -19,10 +60,17 @@ set(ha(2:6),'YTickLabel','')
 
 end
 
-function data_plot_ = readdata(path, data_plot)
-    
-            localpath = pwd;
-            cd(matpath);
+
+function data_plot_ = readdata(L, newL, data_plot)
+            try
+                it = data_plot.iter;
+                ti = data_plot.time;
+                tes = data_plot.test_error;
+            catch
+                it = [];
+                ti = [];
+                tes = [];
+            end
             load(L, 'infos');
             iter = [infos.iter];
             time = [infos.time];
@@ -41,15 +89,14 @@ function data_plot_ = readdata(path, data_plot)
             it = [it; biter];
             ti = [ti; btime];
             tes = [tes; bte];
-        end
-    end
 
 
-    data_plot.beta = b;
-    data_plot.index = ii;
-    data_plot.tsize = s;
-    data_plot.trank = r;
-    data_plot.OS = OS;
+
+    %data_plot.beta = b;
+    %data_plot.index = ii;
+    %data_plot.tsize = s;
+    %data_plot.trank = r;
+    %data_plot.OS = OS;
     data_plot.iter = it;
     data_plot.time = ti;
     data_plot.test_error = tes;
@@ -69,3 +116,4 @@ function [L_filenameinfo, newL_filenameinfo]= create_beta_name(foldername, b_ind
         filenameinfo = [foldername, u, 'KM_', method, 'info', '.mat'];
     end
 end
+
